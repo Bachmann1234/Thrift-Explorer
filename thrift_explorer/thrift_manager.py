0adeab62e0750789542a10348653058bc9920335
Matt Bachmann <bachmann.matt@gmail.com>
2018-07-20 08:58:43 -0400

    Move request translation to its own module

--- thrift_explorer/thrift_manager.py
+++ thrift_explorer/thrift_manager.py
@@ -2,46 +2,15 @@ import glob
 import os
 
 import thriftpy
-from thrift_explorer.communication_models import Protocol, Transport
-from thrift_explorer.thrift_models import BaseType
-from thrift_explorer.thrift_parser import parse_service_specs
 from thriftpy.protocol import (
     TBinaryProtocolFactory,
     TCompactProtocolFactory,
     TJSONProtocolFactory,
 )
 
-
-def _translate_arg(arg_spec, raw_arg):
-    """
-    translate the raw args into ones the client can handle.
-    Basically this means turning parts of a dictionary into
-    the correct objects for thrift. This is most relevant when 
-    structs are introduced.
-    """
-    if isinstance(arg_spec.type_info, BaseType):
-        return raw_arg
-    else:
-        raise ValueError("OMG")  # I will rethink this
-
-
-def _translate_request_body(endpoint, request_body):
-    """
-    Translate the request dictionary into whatever arguments
-    are required to make the call with the thrift client
-
-    endpoint: The endpoint spec as parsed by ThriftManager 
-
-    request_dict: dictionary containing the args that should match
-    the service. The values should have been validated before hitting 
-    this method
-    """
-    processed_args = {}
-    for arg_spec in endpoint.args:
-        processed_args[arg_spec.name] = _translate_arg(
-            arg_spec, request_body[arg_spec.name]
-        )
-    return processed_args
+from thrift_explorer.communication_models import Protocol, Transport
+from thrift_explorer.request_translator import translate_request_body
+from thrift_explorer.thrift_parser import parse_service_specs
 
 
 def _load_thrifts(thrift_directory):
@@ -90,7 +59,7 @@ class ThriftManager(object):
             thrift_spec = self.service_specs[thrift_request.thrift_file]
             service = thrift_spec[thrift_request.service_name]
             return getattr(client, thrift_request.endpoint_name)(
-                **_translate_request_body(
+                **translate_request_body(
                     service.endpoints[thrift_request.endpoint_name],
                     thrift_request.request_body,
                 )
