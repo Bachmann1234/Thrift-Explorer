c72df4e68355ad2c10ceedeb0ada8061e5729281
Matt Bachmann <bachmann.matt@gmail.com>
2018-10-01 23:56:24 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = "https://github.com/Bachmann1234/thriftExplorer"
 EMAIL = "bachmann.matt@gmail.com"
 AUTHOR = "Matt Bachmann"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = .2
+VERSION = .3
 
 # What packages are required for this module to be executed?
 REQUIRED = ["thriftpy", "attrs"]
