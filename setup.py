f11bd0f4b83550a8aadbaac876eb3e87bc16bbb7
Matt Bachmann <bachmann.matt@gmail.com>
2018-10-10 23:30:11 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = "https://github.com/Bachmann1234/thriftExplorer"
 EMAIL = "bachmann.matt@gmail.com"
 AUTHOR = "Matt Bachmann"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = 0.6
+VERSION = 0.7
 
 # What packages are required for this module to be executed?
 REQUIRED = ["thriftpy", "attrs", "flask"]
